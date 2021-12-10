b3f96f2237c6640e3e0a152d4ca2801bd5d63dd4
Jaeseung Choun <54395509+lovelyAlien@users.noreply.github.com>
2021-12-10 18:56:08 +0900

    Give up BM Algorithm

---
+++ src/sort_algorithm/BMAlgorithm.java
@@ -0,0 +1,61 @@
+package sort_algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class BMAlgorithm {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] strArr = str.toCharArray();
+        String sub = sc.nextLine();
+        char[] subArr = sub.toCharArray();
+        int[] skip=initSkip(subArr);
+
+
+        List<Integer> answer= new ArrayList<>();
+        solution(strArr, subArr,answer,skip);
+
+
+    }
+
+    private static int[] initSkip(char[] subArr) {
+        int[] skip=new int[27];
+        for(int i=0; i<27;i++){
+            skip[i]=-1;
+        }
+
+        for(int i=subArr.length-1;i>=0;i--){
+            if(skip[i]==-1)
+                skip[(int)subArr[i]-(int)'A']=subArr.length-1-i;
+        }
+        return skip;
+    }
+
+    private static List<Integer> solution(char[] strArr, char[] subArr, List<Integer> answer, int[] skip) {
+        int i=subArr.length-1;
+        int j=subArr.length-1;
+
+        while(i<strArr.length){
+            if(j==-1){
+                j=subArr.length-1;
+                i=i+subArr.length+1;
+
+            }
+
+            if(strArr[i]==subArr[j]){
+                i=i-1;
+                j=j-1;
+            }
+            else{
+                int gap=skip[(int)strArr[i]];
+
+            }
+        }
+        return null;
+
+    }
+}
--- src/sort_algorithm/KMPAlgorithm.java
+++ src/sort_algorithm/KMPAlgorithm.java
@@ -1,4 +1,78 @@
 package sort_algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class KMPAlgorithm {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        char[] strArr = str.toCharArray();
+        String sub = sc.nextLine();
+        char[] subArr = sub.toCharArray();
+
+
+        int[] table = makeTable(subArr);
+
+        System.out.println(Arrays.toString(table));
+        List<Integer> answer = new ArrayList<>();
+        solution(strArr, subArr, table, answer);
+
+
+    }
+
+    private static List<Integer> solution(char[] strArr, char[] subArr, int[] table, List<Integer> answer) {
+
+        int j = 0;
+        for (int i = 0; i < strArr.length; i++) {
+            while (strArr[i] != subArr[j] && j > 0) {
+                j = table[j - 1];
+            }
+
+            if (strArr[i] == subArr[j]) {
+                if (j == subArr.length - 1) {
+                    answer.add(i - j);
+                    j = table[j];
+                } else
+                    ++j;
+
+            }
+
+
+        }
+
+        System.out.println(answer.toString());
+        return answer;
+
+    }
+
+    private static int[] makeTable(char[] subArr) {
+
+        int len = subArr.length;
+
+        int[] table = new int[len];
+
+        int j = 0;
+
+        for (int i = 1; i < len; i++) {
+
+
+            while (j > 0 && subArr[i] != subArr[j]) {
+                j = table[j - 1];
+            }
+            if (subArr[i] == subArr[j]) {
+                table[i] = ++j;
+            }
+
+
+        }
+
+        return table;
+
+
+    }
+
+
 }
