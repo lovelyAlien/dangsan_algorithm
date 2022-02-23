b7a43ad13885f2f1835335d6b060a994289193ad
lovelyAlien <chunzasang@gmail.com>
2022-02-23 16:36:10 +0900

    Solve the programmers_problem86052

---
+++ src/jason/programmers/problem86052/Problem86052.java
@@ -0,0 +1,14 @@
+package jason.programmers.problem86052;
+
+import java.util.Arrays;
+
+public class Problem86052 {
+
+    public static void main(String[] args) {
+        String[] grid={"SL","LR"};
+
+        int[] answer=new Solution().solution(grid);
+        System.out.println(Arrays.toString(answer));
+    }
+
+}
